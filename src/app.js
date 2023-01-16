252dcacb9b491dad52433e4efb3b9544cdf59e29
cnaranjo27 <cnaranjo@fullmotos.cl>
2023-01-16 23:45:42 +0000

    generador excucasa

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["El Perro", "Mi Abuela", "Su tortuga", "Mi pajaro"];
+let action = ["comió", "orinó", "aplastó", "rompió"];
+let what = ["mi tarea", "las llaves", "mi ferrari"];
+let when = ["antes de clases", "justo a tiempo", "cuando dormía"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusa = excuse(who, action, what, when);
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = excusa;
 };
+
+function excuse(array1, array2, array3, array4) {
+  let whoRandomNumber = Math.floor(Math.random() * array1.length);
+  let actionRandomNumber = Math.floor(Math.random() * array2.length);
+  let whatRandomNumber = Math.floor(Math.random() * array3.length);
+  let whenRandomNumber = Math.floor(Math.random() * array4.length);
+
+  return (
+    array1[whoRandomNumber] +
+    " " +
+    array2[actionRandomNumber] +
+    " " +
+    array3[whatRandomNumber] +
+    " " +
+    array4[whenRandomNumber]
+  );
+}
